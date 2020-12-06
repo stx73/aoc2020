0cb2d5415fbabe6c73bb846167eda4884e7c0420
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2020-12-06 13:51:15 -0500

    day6

---
+++ day6/2.sh
@@ -0,0 +1,19 @@
+gawk -v RS='' -v FS='[ \t\n]' -v OFS=, '{ $1 = $1 } ; 1' input | python3 <(cat <<-EOPY
+import fileinput
+from typing import Optional
+
+
+def main(input_file: fileinput.FileInput) -> Optional[int]:
+    cnt = 0
+    for line in input_file:
+        cnt += len(
+            set.intersection(*[set(answer) for answer in line.strip().split(",")])
+        )
+
+    return cnt
+
+
+if __name__ == "__main__":
+    print(main(fileinput.input()))
+EOPY
+)
