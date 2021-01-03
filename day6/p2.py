056a4a65162737e1472a165250297de19ba25511
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-01-03 16:35:08 +0100

    day6: replace with Python

---
+++ day6/p2.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+sections: List[str] = []
+Group = List[str]
+groups: List[Group] = []
+with open("input") as input_file:
+    sections += input_file.read().rstrip().split("\n\n")
+    groups = list(map(lambda text: text.splitlines(), sections))
+
+print(sum(len(set.intersection(*map(set, group))) for group in groups))
