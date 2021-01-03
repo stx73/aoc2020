5f5cba5b3bc0e1468ac3b230aa87b8903b99bcb3
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-01-03 16:17:56 +0100

    day5

---
+++ day5/p2.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+sections: List[str] = []
+ID = int
+seats: List[ID] = []
+with open("input") as input_file:
+    sections += input_file.read().rstrip().split()
+    seats = list(
+        map(
+            lambda seat: ID(seat.translate(str.maketrans("FLBR", "0011")), base=2),
+            sections,
+        )
+    )
+
+print((set(range(min(seats), max(seats))) - set(seats)).pop())
