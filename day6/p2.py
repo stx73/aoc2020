b0920c119efb5bd4584d2145d8846ea2ceb481b5
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-01-03 17:01:39 +0100

    See https://github.com/python/mypy/issues/6697

--- day6/p2.py
+++ day6/p2.py
@@ -8,4 +8,4 @@ with open("input") as input_file:
     sections += input_file.read().rstrip().split("\n\n")
     groups = list(map(lambda text: text.splitlines(), sections))
 
-print(sum(len(set.intersection(*map(set, group))) for group in groups))
+print(sum(len(set.intersection(*map(lambda x: set(x), group))) for group in groups))
